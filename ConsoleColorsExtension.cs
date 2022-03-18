8b6ab2864c7969f0d80ad7047f9774310d149546
Bar Nuri <barn@ke-la.com>
2022-03-18 16:49:32 +0200

    cleanup history

---
+++ ConsoleColorsExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CSharpExtensions.OpenSource.ConsoleColors
+{
+    public static class ConsoleColorsExtension
+    {
+        private const string Reset = "\u001b[0m";
+
+        public static string CleanColors(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) { return str; }
+            var regex = new Regex("\\u001b\\[[\\d]+m", RegexOptions.None);
+            return regex.Replace(str, "");
+        }
+
+        public static string ColorByNumber(this string str, int num) => $"\u001b[{num}m" + str + Reset;
+        public static string Black(this string str) => ColorByNumber(str, 30);
+        public static string Red(this string str) => ColorByNumber(str, 31);
+        public static string Yellow(this string str) => ColorByNumber(str, 33);
+        public static string Green(this string str) => ColorByNumber(str, 32);
+        public static string Blue(this string str) => ColorByNumber(str, 34);
+        public static string Magenta(this string str) => ColorByNumber(str, 35);
+        public static string Cyan(this string str) => ColorByNumber(str, 36);
+        public static string White(this string str) => ColorByNumber(str, 37);
+        public static string BlackBg(this string str) => ColorByNumber(str, 40);
+        public static string RedBg(this string str) => ColorByNumber(str, 41);
+        public static string YellowBg(this string str) => ColorByNumber(str, 43);
+        public static string GreenBg(this string str) => ColorByNumber(str, 42);
+        public static string BlueBg(this string str) => ColorByNumber(str, 44);
+        public static string MagentaBg(this string str) => ColorByNumber(str, 45);
+        public static string CyanBg(this string str) => ColorByNumber(str, 46);
+        public static string WhiteBg(this string str) => ColorByNumber(str, 47);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,38 @@
+# Install
+
+```bash
+Install-Package CSharpExtensions.OpenSource.ConsoleColors
+# or
+dotnet add package CSharpExtensions.OpenSource.ConsoleColors
+```
+
+![Example](ex.png)
+
+# Use
+
+```c#
+using CSharpExtensions.OpenSource.ConsoleColors;
+
+public static void Main(string[] args)
+{
+    Console.WriteLine("text".ColorByNumber(30)); // equals, 30 = black in ansi colors
+    Console.WriteLine("black".Black());
+    Console.WriteLine("red".Red());
+    Console.WriteLine("green".Green());
+    Console.WriteLine("yellow".Yellow());
+    Console.WriteLine("blue".Blue());
+    Console.WriteLine("magenta".Magenta());
+    Console.WriteLine("cyan".Cyan());
+    Console.WriteLine("white".White());
+    Console.WriteLine("blackBg".BlackBg());
+    Console.WriteLine("redBg".RedBg());
+    Console.WriteLine("greenBg".GreenBg());
+    Console.WriteLine("yellowBg".YellowBg());
+    Console.WriteLine("blueBg".BlueBg());
+    Console.WriteLine("magentaBg".MagentaBg());
+    Console.WriteLine("cyanBg".CyanBg());
+    Console.WriteLine("whiteBg".WhiteBg());
+
+    Console.WriteLine("you can merge foreground  color with background color".Black().YellowBg());
+}
+```
